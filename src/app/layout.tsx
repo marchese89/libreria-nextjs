d94e4efecee8208fe7f5a3cdb1c50cef75acfbde
marchese89 <marchese89@hotmail.com>
2023-11-22 07:39:15 +0100

    ...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
+    <html lang="en">
       <body className={inter.className} >{children}</body>
     </html>
   )
